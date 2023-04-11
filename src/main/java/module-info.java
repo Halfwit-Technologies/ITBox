1ae28fb4d67b433e0ad541b2c3ca6625a009f25b
ItsTrashPanda <adam@lvhills.com>
2023-04-10 21:40:42 -0700

    Revert "Initial v2 code"
    This reverts commit 2bed9c1879782fdafd0646f75f1971b214595dcf.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module tech.halfwit.itbox {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens tech.halfwit.itbox to javafx.fxml;
-    exports tech.halfwit.itbox;
-}
--- src/main/java/tech/halfwit/subnetcalculator/HelperFunctions.java
+++
@@ -1,7 +0,0 @@
-package tech.halfwit.subnetcalculator;
-
-public class HelperFunctions {
-    public static double Log2(int x) {
-        return(Math.log(x)/Math.log(2));
-    }
-}
